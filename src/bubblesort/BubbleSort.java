646731ca4007dff7ecbde243673734083a154ea2
ke <ke@B110-17.pslib.cz>
2015-10-07 10:28:10 +0200

    final version (with input/output)

--- src/bubblesort/BubbleSort.java
+++ src/bubblesort/BubbleSort.java
@@ -2,16 +2,31 @@
 
 package bubblesort;
 
+import java.util.Scanner;
+
 public class BubbleSort {
 
     public static void main(String[] args) {
         // uvodní hodnoty pro testování
-        int[] pole = {5, 4, 3, 2, 1};
+        // int[] pole = {5, 4, 3, 2, 1};
 
-        int n = pole.length;
+        // vstup čísel zakončený nulou
+        final int MAXITEM = 100;
+        int[] pole = new int[MAXITEM];
+        Scanner vstup = new Scanner(System.in);
+        System.out.print("Vlož číslo: ");
+        int cislo = vstup.nextInt();
+        int pocet = 0;
+        while (cislo != 0) {
+            pole[pocet] = cislo;
+            pocet++;
+            System.out.print("Vlož číslo: ");
+            cislo = vstup.nextInt();
+        }
+
         // BubbleSort algoritmus
-        for (int j = 1; j <= n - 1; j++) {
-            for (int i = 0; i <= n - j - 1; i++) {
+        for (int j = 1; j <= pocet - 1; j++) {
+            for (int i = 0; i <= pocet - j - 1; i++) {
                 // porovnání aktuální a následující položky v poli
                 if (pole[i] > pole[i + 1]) {
                     // prohození aktuální a následující položky
@@ -21,8 +36,9 @@ public class BubbleSort {
                 }
             }
         }
-        // výpis seřazeného pole
-        for (int i=0; i<pole.length; i++) {
+        
+        System.out.println("Seřazené hodnoty:");
+        for (int i=0; i<pocet; i++) {
             System.out.print(pole[i]+" ");
         }
         System.out.println();
